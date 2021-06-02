f3bdaa277f1d265425bcdbe2eaa673388afbcf37
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-06-02 22:21:32 +0530

    ajsbf

--- DecisionControlAndLoop/1.cpp
+++ DecisionControlAndLoop/1.cpp
@@ -161,9 +161,24 @@ public:
             std::cout << "Number of Zero" << zero << "\n";
         }
     }
+
+    ///Pattersns
+    void p1()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                std::cout<<i+1<<"--"<<j;
+            }
+            std::cout<<"\n";
+            
+        }
+        
+    }
 };
 int main()
 {
     DecisionControlAndLoop d;
-    d.enter_();
+    d.p1();
 }
