5792672731ae9558300af21097d42207f5c911cc
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-06-05 09:00:48 +0530

    asd

--- DecisionControlAndLoop/1.cpp
+++ DecisionControlAndLoop/1.cpp
@@ -292,9 +292,29 @@ public:
         }
         std::cout<<pr;
     }
+    void prime()
+    {
+        int n;
+        std::cin>>n;
+        int count = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if(n%i == 0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            std::cout<<"Prime";
+        }else{
+            std::cout<<"Chal nikal";
+        }
+        
+    }
 };
 int main()
 {
     DecisionControlAndLoop d;
-    d.fact();
+    d.prime();
 }
